773780cfec48a7793460d5a9db21603baa92579e
Pedro Pedrosa <blastamos@outlook.com>
2018-06-15 21:38:50 +0100

    filter implementation

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,7 +1,11 @@
+import * as expr from '../../ts-expressions';
 import * as op from './operation';
+import { ExpressionKind } from 'ts-expressions/lib/expressions/ExpressionKind';
 
 export interface IDataSource<T> {
-    filter: (predicate: (element: T) => boolean) => IDataSource<T>;
+    filter(predicate: expr.Expression<(element: T) => boolean>): IDataSource<T>;
+    filter(predicate: (element: T) => boolean): IDataSource<T>;
+    filter(parameterName: string, predicate: op.QueryOperation): IDataSource<T>;
     //map: <K>(projection: (element: T) => K) => IDataSource<K>;
     //first: () => Promise<T>;
     //any: () => Promise<boolean>;
@@ -20,13 +24,14 @@ export class DataSourceBase<T> implements IDataSource<T> {
     provider: IDataSourceProvider;
     query: op.QueryOperation;
 
-    filter(predicate: (element: T) => boolean): IDataSource<T> {
+    filter(predicateOrParameterName: expr.Expression<(element: T) => boolean> | ((element: T) => boolean) | string, predicateOperation?: op.QueryOperation): IDataSource<T> {
+        const { parameterName, body: predicate } = this.extractLambdaProperties(predicateOrParameterName, predicateOperation);
         return new DataSourceBase<T>(this.provider, 
         {
             operation: op.QueryOperationNodeType.filter,
             source: this.query,
-            parameterName: 'p',
-            predicate: this.query,
+            parameterName,
+            predicate,
         } as op.FilterOperation);
     }
     /*map<K>(predicate: (element: T) => K): IDataSource<K> {
@@ -46,5 +51,26 @@ export class DataSourceBase<T> implements IDataSource<T> {
     }*/
     toArray(): Promise<T[]> {
         return this.provider.execute(this.query);
+    }
+
+    private extractLambdaProperties<K extends {}>(bodyOrParameterName: expr.Expression<K> | K | string, bodyOperation?: op.QueryOperation) {
+        if (typeof bodyOrParameterName == 'string') {
+            return {
+                parameterName: bodyOrParameterName,
+                body: bodyOperation!
+            }
+        }
+        else if (expr.isLambdaExpression(bodyOrParameterName) && bodyOrParameterName.parameters.length == 1) {
+            return {
+                parameterName: bodyOrParameterName.parameters[0].name,
+                body: this.convertExpressionToQueryOperation(bodyOrParameterName.body)
+            }
+        }
+        else {
+            throw new Error('Predicate must be a lambda expression or a query operation');
+        }
+    }
+    private convertExpressionToQueryOperation(e: expr.ExpressionBase): op.QueryOperation {
+        
     }
 }
